295c52ff0644eb4b021accf0ec43d88c9b73f15e
Pi <atelierdepi@gmail.com>
2025-02-05 08:07:23 +0100

    theme enfant
    création du thème enfant et application de celui ci

---
+++ app/public/wp-content/themes/Elementor Child/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/*  a quel moment la fonction doit etre appele wp_enqueue_script, et quel fonciton doit etre appele theme_enqueue_style */
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+}
